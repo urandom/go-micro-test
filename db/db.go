20d0a5768750e71976829ab0a1015d147f8d33f8
Viktor Kojouharov <developer@sugr.org>
2016-11-01 00:35:14 +0200

    Add some docs to the db package

---
+++ db/db.go
@@ -0,0 +1,11 @@
+// Package db provides the service definitions for getting data from an
+// implementation.
+package db
+
+//go:generate protoc --go_out=plugins=micro:. db.proto
+
+const (
+	// DummyDB is the name of the dummy service implementation of the
+	// various handlers.
+	DummyDB = "org.sugr.micro.test.db.dummy"
+)
--- db/doc.go
+++
@@ -1,7 +0,0 @@
-package db
-
-//go:generate protoc --go_out=plugins=micro:. db.proto
-
-const (
-	DummyDB = "org.sugr.micro.test.db.dummy"
-)
